bdeba0045302b8b0f336369ab09a65c2dcd87ba4
R <63073421+ramizwd@users.noreply.github.com>
2023-04-22 19:27:25 +0300

    user route and some comments

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import userRoute from './routes/userRoute';
 import MessageResponse from '../interfaces/MessageResponse';
 
 const router = express.Router();
+
+// Define the route handler for the root path
+router.get<{}, MessageResponse>('/', (_req, res) => {
+  res.json({
+    message: 'routes: users',
+  });
+});
+
+// Mount the user route
+router.use('/users', userRoute);
 
 export default router;
